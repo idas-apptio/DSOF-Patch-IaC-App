2b0606c12b0ffb262c679eb16d1dc79b68290576
idas-apptio <idas@apptio.com>
2023-09-25 21:18:49 +0530

    snyk fixes

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,5 +1,9 @@
 resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
-  enable_logging = false
+  enable_logging = true
+  enable_log_file_validation = true
+  is_multi_region_trail = true
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.my_log_group.arn
+  kms_key_id = "your-kms-key-id"
 }
