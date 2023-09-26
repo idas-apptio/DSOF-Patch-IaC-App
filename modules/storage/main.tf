380a2a7b86fc32a839c95b1a99cfeca00e632569
idas-apptio <idas@apptio.com>
2023-09-26 10:49:55 +0530

    snyk changes

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,5 +1,17 @@
 resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
+  versioning {
+    enabled = true
+  }
+
+  # Check the AWS provider version to determine if the logging block is needed
+  dynamic "logging" {
+    for_each = can(tristate(condition, false, null))
+    content {
+      target_bucket = aws_s3_bucket.logging_bucket.id
+      target_prefix = "logs/"
+    }
+  }
 }
 
 # resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
@@ -12,7 +24,7 @@ resource "aws_s3_bucket" "insecure-bucket" {
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 20
-  encrypted         = false
+  encrypted         = true
   tags = {
     Name = "insecure"
   }
